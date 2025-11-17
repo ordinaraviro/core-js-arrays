aa3797630499294e113fe81b5195865ccbb057a9
ordinaraviro <ordinaraviro@gmail.com>
2025-11-17 13:13:53 +0200

    feat: add shift array func

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -550,8 +550,10 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const len = arr.length;
+  const shift = ((n % len) + len) % len;
+  return arr.slice(-shift).concat(arr.slice(0, len - shift));
 }
 
 /**
