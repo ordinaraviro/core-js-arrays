b770b84fb3bffecad95ef33cdf7f72b6efecc9cf
ordinaraviro <ordinaraviro@gmail.com>
2025-11-17 12:43:37 +0200

    feat: add get average func

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -123,8 +123,13 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+
+  const sum = arr.reduce((total, num) => total + num, 0);
+  const avg = sum / arr.length;
+
+  return Math.round(avg * 100) / 100;
 }
 
 /**
